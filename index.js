85f2b9ddd97f50ae34ccdc003c1fdaf81e2c3d5b
beingbiplov <sharmabiplov@gmail.com>
2022-06-11 16:00:27 +0545

    add different route options

--- index.js
+++ index.js
@@ -6,6 +6,20 @@ app.get('/', (req, res) =>{
     res.send('Hello World!')
 })
 
+app.get('/about', (req, res) => {
+    res.send("About Express!")
+})
+
+// route to match string patterns 
+app.get('/ab?cd', (req, res) => {
+    res.send('This route matched with acd or abcd')
+})
+
+// routes with route parameters 
+app.get('/user/:userId', (req, res) =>{
+    res.send(req.params)
+})
+
 app.listen(port, () => {
     console.log(`Listening on port ${port}`)
 })
